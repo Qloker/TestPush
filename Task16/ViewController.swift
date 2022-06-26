8a3a30deef88e45dd0f1c04e697bb1e123e3314a
Ilia Zakharov <98qloker@gmail.com>
2022-06-26 21:18:41 +0300

    first commit

---
+++ Task16/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Task16
+//
+//  Created by Ilia Zakharov on 26.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Testsssss")
+    }
+
+
+}
+
